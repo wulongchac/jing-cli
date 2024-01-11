8ebbc5e44cd91538fa53c9f827b66fde7d1eeccb
wulongchac <wulongchac@users.noreply.github.com>
2024-01-11 22:21:38 +0000

    first commit

---
+++ src/meta.js
@@ -0,0 +1,9 @@
+const template = {
+  'vue2.x': 'https://github.com/DreamElapse/vue-template.git',
+  'mpvue2.x': 'https://github.com/DreamElapse/mpvue-template.git',
+  'uni-app': 'https://github.com/DreamElapse/uni-app-template.git'
+} 
+
+module.exports = {
+  template
+}
